nitrogen-project/android_device_xiaomi_kenzo

kenzo: Move to fs config gen

BoardConfig.mk
@@ -48,6 +48,8 @@ TARGET_USES_64_BIT_BINDER := true
 
 ENABLE_CPUSETS := true
 
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 ramoops_memreserve=4M androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000

AKabhishek/dnd_mido

Change-Id: I09e3a16a03b94afc5d8df4c8c302976428b518f8
Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

BoardConfig.mk
@@ -159,7 +159,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 TARGET_NO_RPC := true
 
 # Filesystem
-TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(LOCAL_PATH)/android_filesystem_config.h
+TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_mido